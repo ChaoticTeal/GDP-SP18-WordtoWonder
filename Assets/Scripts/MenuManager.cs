5ba7cb2ff7ef8ff3d0bdc361e97285a16192c07f
PurpleNinjaLink <jawhittom@gmail.com>
2018-04-30 20:11:08 -0500

    Minor Fixes 4/30
    Cursor unlocked on end scene loaded, end trigger moved, control description modified.

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -26,6 +26,7 @@ public class MenuManager : MonoBehaviour
     private void Start()
     {
         Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     public void PlayButton()
